d38528ab2c1412691df08f98017427f0e2af65fa
Dexqp <122967278+Dexqp@users.noreply.github.com>
2023-01-23 11:10:08 +0000

    fixed threshold command

--- util/stickycommands.py
+++ util/stickycommands.py
@@ -8,7 +8,8 @@ import discord
 async def command_helpsticky(client, message, config, commands):
     sticky_commands = {
         "setmessage": "Sets the message sent with this command to the current channel and add starts the sticky feature.",
-        "rchannel": "Removes the bot's message from the current channel."
+        "rchannel": "Removes the bot's message from the current channel.",
+        "limit": "sets the number of messages that needs to be sent before the the bot sends a sticky"
     }
     command_list = "List of available sticky commands:\n"
     for command in sticky_commands:
@@ -48,8 +49,8 @@ async def command_limit(client, message, config):
     try:
         new_threshold = int(message.content.split()[1])
         config["threshold"] = new_threshold
-        with open("config.json", "r+") as config_file:
-            config_file.write(json.dumps(config, indent=4))
+        with open("config.json", "w") as config_file:
+            json.dump(config, config_file, indent=4)
         await message.channel.send(f"Threshold set to {new_threshold}")
     except (ValueError, IndexError):
         await message.channel.send("Please provide a valid threshold")
